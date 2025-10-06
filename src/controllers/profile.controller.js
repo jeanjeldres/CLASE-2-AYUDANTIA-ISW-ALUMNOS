4026bb395c3d2b7b93430216c2ca337b1d1b0e92
jeanjeldres <jean.jeldres2101@alumnos.ubiobio.cl>
2025-10-05 21:27:07 -0300

    profile.controller modificado

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,4 +1,7 @@
 import { handleSuccess } from "../Handlers/responseHandlers.js";
+import * as userService from '../services/user.service.js';
+import { handleError } from '../Handlers/responseHandlers.js';
+import bcrypt from 'bcryptjs';
 
 export function getPublicProfile(req, res) {
   handleSuccess(res, 200, "Perfil público obtenido exitosamente", {
@@ -14,3 +17,33 @@ export function getPrivateProfile(req, res) {
     userData: user,
   });
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { email, password } = req.body;
+
+    const newData = {};
+    if (email) newData.email = email;
+    if (password) newData.password = await bcrypt.hash(password, 10);
+
+    const updatedUser = await userService.updateUser(userId, newData);
+
+    if (!updatedUser) return res.status(404).json({ message: 'Usuario no encontrado' });
+    res.json({ message: 'Perfil actualizado con éxito', user: updatedUser });
+  } catch (error) {
+    handleError(res, error);
+  }
+};
+
+export const deleteProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const deletedUser = await userService.deleteUser(userId);
+
+    if (!deletedUser) return res.status(404).json({ message: 'Usuario no encontrado' });
+    res.json({ message: 'Perfil eliminado con éxito' });
+  } catch (error) {
+    handleError(res, error);
+  }
+};
