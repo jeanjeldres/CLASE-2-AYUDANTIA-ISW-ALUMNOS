1bccab576511b0c79a894f28adb14e6b9cc469db
jeanjeldres <jean.jeldres2101@alumnos.ubiobio.cl>
2025-10-05 23:53:06 -0300

    arreglando routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,12 @@
+
+import { Router } from "express";
+import { updateProfileController, deleteProfileController } from "../controllers/user.controller.js";
+import { authMiddleware } from '../middleware/auth.middleware.js';
+
+const router = Router();
+
+router.patch("/profile/private", authMiddleware, updateProfileController);
+
+router.delete("/profile/private", authMiddleware, deleteProfileController);
+
+export default router;
